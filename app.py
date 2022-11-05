205f4fe3235c8157113a678dd81626d2fd86bfa7
Opeyemi Babarinde <vjchris343@gmail.com>
2022-11-05 04:15:30 +0100

    initial commit

---
+++ app.py
@@ -0,0 +1,54 @@
+from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import UserMixin
+import os
+
+base_dir = os.path.dirname(os.path.realpath(__file__))
+
+app = Flask(__name__)   
+
+# db = SQLAlchemy(app)
+# db.init_app(app)
+
+app.config['SECRET_KEY'] = 'secret key'
+app.config["SQLALCHEMY_DATABASE_URI"] = 'sqlite:///' + os.path.join(base_dir, 'db.sqlite')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+db.init_app(app)
+
+
+
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    firstname = db.Column(db.String(20), unique=False, nullable=False)
+    lastname = db.Column(db.String(20), unique=False, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(60), nullable=False)
+
+    def __repr__(self):
+        return f"User('{self.username}', '{self.email}')"
+
+
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+
+@app.route('/signup')
+def register():
+    return render_template('signup.html')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/profile')
+def profile():
+    return render_template('about.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
