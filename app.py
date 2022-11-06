4849fa8d57725c01751317ee9723206eb68fe4b9
Opeyemi Babarinde <65448867+Bishopxxx@users.noreply.github.com>
2022-11-06 08:52:53 +0100

    authentication done

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
-from flask import Flask, render_template, url_for, request, redirect
+from flask import Flask, render_template, url_for, request, redirect, flash
 from flask_sqlalchemy import SQLAlchemy
-from flask_login import UserMixin
+from flask_login import UserMixin, current_user, login_user, LoginManager, login_required, logout_user
+from werkzeug.security import generate_password_hash, check_password_hash
 import os
 
 base_dir = os.path.dirname(os.path.realpath(__file__))
@@ -12,7 +13,13 @@ app.config["SQLALCHEMY_DATABASE_URI"] = 'sqlite:///' + os.path.join(base_dir, 'd
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
-# db.init_app(app)
+db.init_app(app)
+
+login_manager = LoginManager(app)
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
 
 
 class User(db.Model, UserMixin):
@@ -32,13 +39,54 @@ class User(db.Model, UserMixin):
 def home():
     return render_template("home.html")
 
-@app.route('/signup')
-def register():
-    return render_template('signup.html')
 
-@app.route('/login')
+@app.route("/signup", methods=["GET", "POST"])
+def signup():
+    if request.method == "POST":
+        email = request.form.get("email")
+        firstname = request.form.get("firstname")
+        lastname = request.form.get("lastname")
+        username = request.form.get("username")
+        password = request.form.get("password")
+        password1 = request.form.get("password1")
+
+        user = User.query.filter_by(email=email).first()
+
+        if user:
+            flash("Email address already exists.", category="error")
+            return redirect(url_for("signup"))
+        elif password != password1:
+            flash("Passwords don't match", category="error")
+        else:
+            
+            new_user = User(email=email, firstname=firstname, lastname=lastname, username=username, password=generate_password_hash(password, method='sha256'))
+
+            db.session.add(new_user)
+            db.session.commit()
+            login_user(new_user)
+            return redirect(url_for("home"))
+            
+    return render_template("signup.html", user=current_user)
+
+@app.route("/login", methods=["GET", "POST"])
 def login():
-    return render_template('login.html')
+    if request.method == "POST":
+        email = request.form.get("email")
+        password = request.form.get("password")
+        user = User.query.filter_by(email=email).first()
+        if user:
+            if check_password_hash(user.password, password):
+                flash("Logged in successfully!", category="success")
+                login_user(user)
+                return redirect (url_for("home"))
+            else:
+                flash("Incorrect password, try again.", category="error")
+        else:
+            flash("Email does not exist.", category="error")
+
+        
+    return render_template("login.html", user=current_user)
+
 
 @app.route('/profile')
 def profile():
@@ -46,6 +94,7 @@ def profile():
 
 @app.route('/logout')
 def logout():
+    logout_user()
     return render_template('home.html')
 
 
